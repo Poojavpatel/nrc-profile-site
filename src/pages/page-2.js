726e4f67c7a8e9343f5ba505f4f8da449cdad07d
Pooja Patel <poojavpatel71@gmail.com>
2020-02-23 18:30:02 +0530

    fetched data from an api using axios,added gitattributes file

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,16 +1,36 @@
 import React from "react"
 import { Link } from "gatsby"
+import axios from 'axios';
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
-)
+class SecondPage extends React.Component{
+  state = {
+    loading:true,
+    error:false,
+    fetchedData : [],
+  }
+  async componentDidMount(){
+    const response = await axios.get('https://swapi.co/api/people/');
+    if(response && response.data.results){
+      this.setState({
+        fetchedData: response.data.results,
+        loading:false,
+      })
+    }
+  }
+  render(){
+    const {fetchedData} = this.state;
+    return(
+      <Layout>
+        <SEO title="Page two" />
+        <h1>Hi from the second page</h1>
+        {fetchedData.map(person =>  <p key={person.name}>{person.name}</p> )}
+        <Link to="/">Go back to the homepage</Link>
+      </Layout>
+    )
+  }
+}
 
 export default SecondPage
